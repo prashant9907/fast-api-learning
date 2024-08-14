19535cca7d64206b950a578f4ddaada3e94ad9b8
prashant9907 <karwasraprashant10@gmail.com>
2024-08-14 11:01:57 +0530

    add user get and tags

--- blog/main.py
+++ blog/main.py
@@ -18,7 +18,7 @@ def get_db():
         db.close()
 
 
-@app.post("/blog", status_code=status.HTTP_201_CREATED)
+@app.post("/blog", status_code=status.HTTP_201_CREATED, tags=['blogs'])
 def create(request: schemas.Blog, db:Session=Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -26,7 +26,7 @@ def create(request: schemas.Blog, db:Session=Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=['blogs'])
 def destroy(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id==id)
     
@@ -49,13 +49,13 @@ def destroy(id, db: Session = Depends(get_db)):
 #     db.commit()
 #     return "updated"
 
-@app.get('/blog')
+@app.get('/blog', tags=['blogs'])
 def all(db: Session=Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}', status_code=200)
+@app.get('/blog/{id}', status_code=200, tags=['blogs'])
 def show(id, response: Response, db:Session=Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -71,7 +71,7 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
 
 
-@app.post('/user', status_code=status.HTTP_201_CREATED)
+@app.post('/user', response_model=schemas.ShowUser,  status_code=status.HTTP_201_CREATED, tags=['users'])
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
     new_user = models.User(name=request.name, email=request.email, password= pwd_context.hash(request.password))
     # new_user = models.User(name=request.name, email=request.email, password= Hash.hash_with_bcrypt(request.password))
@@ -79,3 +79,10 @@ def create_user(request: schemas.User, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@app.get('/user/{id}', response_model=schemas.ShowUser, tags=['users'])
+def get_user(id:int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with the {id} is not found")
+    return user
--- blog/schemas.py
+++ blog/schemas.py
@@ -5,8 +5,17 @@ class Blog(BaseModel):
     title: str
     body: str
 
+    class Config():
+        orm_mode = True
+
 
 class User(BaseModel):
     name: str
     email: str
     password:str
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+    class Config():
+        orm_mode = True
